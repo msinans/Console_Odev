3ecef76fa77c533cf0f362693ac60d264068d4c4
m.sinans <msomercikoglu@gmail.com>
2023-03-11 11:31:07 +0300

    Add project files.

---
+++ Console_Odev/Kisi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Odev
+{
+    internal class Kisi
+    {
+        public void kisibilgileri()
+        {
+
+        }
+    }
+}
